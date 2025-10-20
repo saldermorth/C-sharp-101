60a8e1860d05b1e4aa0af341f2796627f1d9ad69
Gustav Berg <berg_gustav@hotmail.com>
2025-10-20 17:44:42 +0200

    #init

---
+++ GenericComparison/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] heltal = { 1, 2, 3 };
+        double[] decimaltal = { 1.0, 2.0, 3.0 };
+        string[] ord = { "1", "2", "3" };
+
+        // Skriva ut med tre olika metoder
+        SkrivUt(heltal);
+        SkrivUt(decimaltal);
+        SkrivUt(ord);
+
+        // Visa mängden kod
+        // Här har vi tre nästan identiska metoder, vilket leder till kodupprepning
+
+        // Om vi behöver ändra utskriftslogiken måste vi ändra på tre ställen
+
+        // Gör en generisk metod
+        SkrivUtGenerisk(heltal);
+        SkrivUtGenerisk(decimaltal);
+        SkrivUtGenerisk(ord);
+    }
+
+    public static void SkrivUt(int[] talen)
+    {
+        foreach (var item in talen)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public static void SkrivUt(double[] talen)
+    {
+        foreach (var item in talen)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public static void SkrivUt(string[] talen)
+    {
+        foreach (var item in talen)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+    // Generisk metod
+    public static void SkrivUtGenerisk<T>(T[] talen)
+    {
+        foreach (var item in talen)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zoo<Cat> catZoo = new Zoo<Cat>();
+            catZoo.AddAnimal(new Cat { Name = "Whiskers" });
+            catZoo.AddAnimal(new Cat { Name = "Mittens" });
+
+            Zoo<Dog> dogZoo = new Zoo<Dog>();
+            dogZoo.AddAnimal(new Dog { Name = "Buddy" });
+            dogZoo.AddAnimal(new Dog { Name = "Daisy" });
+
+            catZoo.AllAnimalsMakeSound();
+            dogZoo.AllAnimalsMakeSound();
+        }
+    }
+    public interface IAnimalFeeder<T> where T : Animal
+    {
+        void FeedAnimal(T animal);
+    }
+
+    public class CatFeeder : IAnimalFeeder<Cat>
+    {
+        public void FeedAnimal(Cat animal)
+        {
+            Console.WriteLine($"Feeding cat: {animal.Name}");
+            // Code to feed the cat
+        }
+    }
+
+    public class DogFeeder : IAnimalFeeder<Dog>
+    {
+        public void FeedAnimal(Dog animal)
+        {
+            Console.WriteLine($"Feeding dog: {animal.Name}");
+            // Code to feed the dog
+        }
+    }
+
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public abstract void MakeSound();
+    }
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
+    public class Zoo<T> where T : Animal
+    {
+        private List<T> animals = new List<T>();
+
+        public void AddAnimal(T animal)
+        {
+            animals.Add(animal);
+        }
+
+        public void AllAnimalsMakeSound()
+        {
+            foreach (T animal in animals)
+            {
+                animal.MakeSound();
+            }
+        }
+    }
+}
