f7cf7d8a711905de56e990e89454aba37c45266c
Gustav Berg <gustav.berg@xlent.se>
2025-10-21 10:47:42 +0200

    List demo

---
+++ ForeachDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] färger = { "röd", "grön", "blå", "gul" };
+Console.WriteLine("Tillgängliga färger:");
+foreach (var färg in färger)
+{
+    Console.WriteLine(färg);
+}
+
+
+double[] veckansTemperaturer = { 22.5, 25.3, 18.9, 20.1, 23.4, 19.8, 21.2 };
+double summa = 0;
+int antalDagar = 0;
+double varmastaTemperatur = double.MinValue;
+
+foreach (double temperatur in veckansTemperaturer)
+{
+    summa += temperatur;
+    antalDagar++;
+    if (temperatur > varmastaTemperatur)
+    {
+        varmastaTemperatur = temperatur;
+    }
+    Console.WriteLine($"Dag {antalDagar}: {temperatur:F1}°C");
+}
+
+double medeltemperatur = summa / antalDagar;
+Console.WriteLine($"\nMedeltemperatur för veckan: {medeltemperatur:F1}°C");
+Console.WriteLine($"Varmaste dagen var: {varmastaTemperatur:F1}°C");
---
+++ ListDemo/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace ListDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skapa listor
+
+            List<int> list = new List<int>();
+
+            List<string> list2 = new();
+
+
+            //Lägg till objekt
+
+            list.Add(1);
+
+
+            //Lägg till en miljon objekt
+
+            for (int i = 1; i < 1_000_000; i++)
+
+            {
+
+                list.Add(i);
+
+            }
+
+
+            //Skriv ut index 800000
+
+            Console.WriteLine(list.ElementAt(800_000));
+
+
+            //Sortera fallande
+
+            list.Reverse();
+
+
+            //Skriv ut igen
+
+            Console.WriteLine(list.ElementAt(800_000));
+
+
+            //Sortera
+
+            list.Sort();
+
+
+            //Ta bort element - Elementet tas bort och alla element efter "hoppar till vänster"
+
+            list.RemoveAt(800_000);
+
+            Console.WriteLine(list.ElementAt(800_000));
+
+
+            //Antalet objekt
+
+            Console.WriteLine(list.Count);
+
+
+            //Använd med foreach
+
+            foreach (var item in list)
+
+            {
+
+                Console.WriteLine("NR: " + item);
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
