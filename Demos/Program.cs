3fce34c2c91dfe9c8dd331b736559748305cf35a
Gustav Berg <gustav.berg@xlent.se>
2025-10-21 09:03:54 +0200

    Added demo

---
+++ Demos/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Demos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public interface IVehicle
+    {
+        void Start();
+        int MaxSpeed { get; }
+    }
+
+    class car : IVehicle
+    {
+        public int MaxSpeed => throw new NotImplementedException();
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
